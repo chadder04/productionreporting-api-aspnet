fe197793b1e910e9a678275e9526969960b97ab1
Chad Garrett <chadgarrett_@hotmail.com>
2023-06-18 11:20:37 -0600

    Init

---
+++ DB.cs
@@ -0,0 +1,52 @@
+﻿namespace ProductionReporting.DB;
+
+public record ProductionRecord
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    // public Date Date { get; set; }
+}
+
+public class ProductionDB
+{
+    private static List<ProductionRecord> _records = new List<ProductionRecord>()
+   {
+     new ProductionRecord{ Id=1, Name="Montemagno, ProductionRecord shaped like a great mountain" },
+     new ProductionRecord{ Id=2, Name="The Galloway, ProductionRecord shaped like a submarine, silent but deadly"},
+     new ProductionRecord{ Id=3, Name="The Noring, ProductionRecord shaped like a Viking helmet, where's the mead"}
+   };
+
+    public static List<ProductionRecord> GetProductionRecords()
+    {
+        return _records;
+    }
+
+    public static ProductionRecord? GetProductionRecord(int id)
+    {
+        return _records.SingleOrDefault(ProductionRecord => ProductionRecord.Id == id);
+    }
+
+    public static ProductionRecord CreateProductionRecord(ProductionRecord ProductionRecord)
+    {
+        _records.Add(ProductionRecord);
+        return ProductionRecord;
+    }
+
+    public static ProductionRecord UpdateProductionRecord(ProductionRecord update)
+    {
+        _records = _records.Select(ProductionRecord =>
+        {
+            if (ProductionRecord.Id == update.Id)
+            {
+                ProductionRecord.Name = update.Name;
+            }
+            return ProductionRecord;
+        }).ToList();
+        return update;
+    }
+
+    public static void RemoveProductionRecord(int id)
+    {
+        _records = _records.FindAll(ProductionRecord => ProductionRecord.Id != id).ToList();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Production Reporting API", Description = "Reporting your Production Information", Version = "v1" });
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Production Reporting API V1");
+});
+
+app.MapGet("/", () => "Hello World!");
+
+
+app.Run();
